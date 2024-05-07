584643064c127bf463150f6e05204343bc820307
4L3X06 <167144057+4L3X06@users.noreply.github.com>
2024-05-06 21:49:52 -0300

    encapsulamento e uso do get e set

--- src/exemplos/TesteConta.java
+++ src/exemplos/TesteConta.java
@@ -5,12 +5,18 @@ public class TesteConta {
         Conta c = new Conta();
         c.depositar(100);
         c.depositar(50);
+        c.setNomeTitular("laís");
+        c.setNumero(1234);
         boolean saqueEfetuado = c.sacar(200);
         if (saqueEfetuado){
             System.out.println("Saque efetuado com sucesso!");
     }  else {
-            System.out.println("ERRO AO SACAR!!!");
+            System.err.println("ERRO AO SACAR!!!");
         }
-        System.out.println("Saldo: " + c.saldo);
+        System.out.println("Nome Titular: " + c.getNomeTitilar());
+        System.out.println("Número: " + c.getNumero());
+        System.out.println("Saldo: " + c.getSaldo()) ;
+        System.out.println(c);
+        
     }
 }
